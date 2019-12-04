85debb1dcb4cbeea8ad5c06a439b4c335ecb082c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 01:44:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
 
   def apply_discount
     if @discount > 0
-      @discount = (@discount / 100)
+      @discount = (@discount / 100) * @total
       @total -= @discount
       puts "Discount applied!"
     else
