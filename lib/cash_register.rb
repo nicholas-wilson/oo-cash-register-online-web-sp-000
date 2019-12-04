9464f043837b97542571c4725834b5a396d7e94b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 01:23:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :employee_discount
 
-  def initialize(discount = false)
+  def initialize(discount = 0)
     @total = 0
     @employee_discount = discount
   end
