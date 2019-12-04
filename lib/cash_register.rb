7ef3fe336ef36e835f220d4acb016a75f7ff295d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 01:29:30 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,16 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
+
+  def add_item(title, price)
+    @items << title
+    @total += price
+  end
+
+  def
 end
