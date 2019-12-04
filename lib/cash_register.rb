e23a63a23d0ad034d7d5c2815e2e8417e1354881
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 01:32:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount, :items
+  attr_accessor :total, :discount, :items, :previous_total
 
   def initialize(discount = 0)
     @total = 0
@@ -7,9 +7,14 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title, price)
+  def add_item(title, price, quantity = 1)
+    @previous_total = @total
     @items << title
-    @total += price
+    if quantity > 1
+      @total += (price * quantity)
+    else
+      @total += price
+    end
   end
 
 end
